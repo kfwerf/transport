c3afe5273fe5188cb3225898e0f1bd74669191ab
Kenneth van der Werf <kennethwerf@gmail.com>
2019-12-31 17:34:46 +0100

    Move filtered methods to utils
    Moved methods for filtering to utils. Added additional methods for
    normalization and matching with normalization.
    
    - Moved methods to utils
    - Added tests
    - Added equal methods

--- transport/src/transport/utils/index.js
+++ transport/src/transport/utils/index.js
@@ -1,1 +1,39 @@
 export const removeDupes = (val, idx, arr) => arr.indexOf(val) === idx;
+
+export const getNormalized = (value = '') => value?.trimLeft()?.trimRight()?.toLowerCase();
+
+export const equalString = (a, b) => getNormalized(a) === getNormalized(b);
+export const equalArray = (arr, b) => arr.map((a) => getNormalized(a)).indexOf(getNormalized(b)) !== -1;
+
+export const getFilteredList = ({list = [], type, color, brand}) => [...list]
+    .filter((vehicle) => !type || equalString(vehicle?.type, type))
+    .filter((vehicle) => !color || equalArray(vehicle?.colors, color))
+    .filter((vehicle) => !brand || equalString(vehicle?.brand, brand));
+
+export const getFilteredLists = (list = [], color = '', brand = '', type = '') => {
+    const filtered = getFilteredList({list, color, brand, type});
+    return {
+        filtered,
+        colors: getFilteredList({list, color: '', brand, type})
+            .map((vehicle) => vehicle?.colors)
+            .reduce((a, b) => a.concat(b), [])
+            .map((color) => getNormalized(color))
+            .filter(removeDupes),
+        brands: getFilteredList({list, color, brand: '', type})
+            .map((vehicle) => vehicle?.brand)
+            .map((brand) => getNormalized(brand))
+            .filter(removeDupes),
+        types: getFilteredList({list, color, brand, type: ''})
+            .map((vehicle) => vehicle?.type)
+            .map((type) => getNormalized(type))
+            .filter(removeDupes),
+    };
+};
+
+export const getStateWithFilteredLists = (state = {}) => {
+    const {transport: list, color, brand, type} = state;
+    return {
+        ...state,
+        ...getFilteredLists(list, color, brand, type),
+    };
+};
--- transport/src/transport/utils/index.test.js
+++ transport/src/transport/utils/index.test.js
@@ -1,6 +1,90 @@
-import { removeDupes } from './index';
+import {
+    equalArray,
+    equalString,
+    getFilteredList,
+    getFilteredLists,
+    getNormalized,
+    removeDupes
+} from "./index";
+const testList = [{
+    type: 'Car',
+    colors: ['gReEn', 'blue'],
+    brand: 'vOlvo',
+}, {
+    type: 'train',
+    colors: ['rEd', 'green '],
+    brand: 'electrolux',
+}, {
+    type: 'airPlane',
+    colors: ['red', ' bLue'],
+    brand: 'aiRbus',
+}, {
+    type: 'cAr',
+    colors: ['green', 'blue'],
+    brand: 'voLvo ',
+}];
 
 test('removes duplicates from a list', () => {
-  const result = ['red', 'red', 'blue', 'red', 'orange', 'r3d'].filter(removeDupes);
-  expect(result).toEqual(['red', 'blue', 'orange', 'r3d']);
+    const result = ['red', 'red', 'blue', 'red', 'orange', 'r3d'].filter(removeDupes);
+    expect(result).toEqual(['red', 'blue', 'orange', 'r3d']);
+});
+
+test('normalizes a string', () => {
+    const result = getNormalized('greEn  ');
+    expect(result).toEqual('green');
+});
+
+test('matches a string', () => {
+    const result = equalString(' GreeN', 'green');
+    expect(result).toBe(true);
+});
+
+test('matches within an array', () => {
+    const result = equalArray(['red', ' GreeN'], 'green');
+    expect(result).toBe(true);
+});
+
+test('filters a list on type', () => {
+    const list = [...testList];
+    const type = 'train';
+    const result = getFilteredList({ list, type });
+
+    expect(result.length).toEqual(1);
+});
+
+test('filters a list on color', () => {
+    const list = [...testList];
+    const color = 'greeN';
+    const result = getFilteredList({ list, color });
+
+    expect(result.length).toEqual(3);
+});
+test('filters a list on brand', () => {
+    const list = [...testList];
+    const brand = 'volvo';
+    const result = getFilteredList({ list, brand });
+
+    expect(result.length).toEqual(2);
+});
+
+test('filtered lists on brand', () => {
+    const list = [...testList];
+    const brand = 'volvo';
+    const result = getFilteredLists(list, '', brand, '');
+
+    expect(result).toEqual({
+        brands: ['volvo', "electrolux", "airbus"],
+        colors: ["green", "blue"],
+        filtered: [{
+                brand: 'vOlvo',
+                colors: ['gReEn', 'blue'],
+                type: 'Car',
+            }, {
+                brand: 'voLvo ',
+                colors: ['green', 'blue'],
+                type: 'cAr',
+            },
+        ],
+        types: ['car'],
+    });
 });
